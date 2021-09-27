049cd1111d5046c268248a56aeb1bff4663bd21e
Rustam Yuburov <rt138328@gmail.com>
2021-09-27 16:18:26 +0600

    Added file where same code written with Express

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/about', (req, res) => {
+  return res.sendFile(path.join(__dirname, '/about.html'));
+});
+
+app.get('/contact-me', (req, res) => {
+  return res.sendFile(path.join(__dirname, '/contact-me.html'));
+});
+
+app.get('*', (req, res) => {
+  return res.sendFile(path.join(__dirname, '/404.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
